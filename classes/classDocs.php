6f53e3f747e0ab7a59048d210ee33d3e44a87955
Alex Schavareto <schavareto@yahoo.com.br>
2022-05-17 17:07:40 -0300

    Segundo commit

---
+++ classes/classDocs.php
@@ -0,0 +1,11 @@
+<?php
+
+    Class Documentos {
+        static function smartlabeldoc_download(){
+              echo "https://docs.google.com/uc?export=download&id=1cSgD1TlRmGnXmpyZxMDbmv0ZlahWK_OE";
+        }
+        static function smartlabeldoc_visualizar(){
+            echo "https://drive.google.com/file/d/1cSgD1TlRmGnXmpyZxMDbmv0ZlahWK_OE/view?usp=sharing";
+      }
+    }
+?>
--- documentacao.php
+++ documentacao.php
@@ -1,9 +1,20 @@
 <?php 
     require_once("assets/head.php");
+    require_once("classes/classDocs.php");
+
+    $obj = new Documentos;
+    
 
 ?>
+<div class="container">
 
-<button type="button" class="btn btn-primary btn-lg">Large button</button>
-<a href="https://unsplash.com/photos/msvZcVAaN-M/download?force=true" download>Baixar PDF
-    
-</a>
+<h1>Donwload Documentações</h1><br>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+<a href=<?php Documentos::smartlabeldoc_download(); ?>> Download </a><br>
+<a href=<?php Documentos::smartlabeldoc_visualizar(); ?> target="_blank"> Visualizar </a>
+
+
+
+
