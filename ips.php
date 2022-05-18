05f1eb14d36e4b538698d53aed02f4f7a523a3f3
Alex Schavareto <schavareto@yahoo.com.br>
2022-05-18 17:45:32 -0300

    tela de ip

---
+++ ips.php
@@ -0,0 +1,72 @@
+<?php
+    require_once("assets/head.php");
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<div class="container">
+<br>
+<h3>IP's para liberação</h3>
+
+<div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        LinkApi
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      <strong>13.90.231.129</strong><br>
+      <strong>35.174.51.251</strong><br> 
+      <strong>52.21.113.85</strong><br>
+      <strong>54.209.233.223</strong><br>
+      <strong>13.82.237.106</strong><br>
+      <strong>40.114.53.254</strong><br>
+      <strong>40.121.3.85</strong><br>
+
+    </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Notazz
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      <strong>45.162.229.158</strong><br>
+      <strong>45.162.228.130</strong><br>
+    </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        SHL
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      <strong>34.231.206.62</strong><br>
+      <strong>34.232.5.19</strong><br>
+      <strong>187.75.202.55</strong><br>
+      <strong>177.101.146.48</strong><br>
+      <strong>186.233.148.188</strong><br>
+      <strong>191.243.199.186</strong><br>
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingFour">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+        IP da Total Express
+      </button>
+    </h2>
+    <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      <strong>IP FIXO TOTAL : </strong> 177.71.198.110 <br>
+      <pre>Liberar pelo nosso DNS: edi.totalexpress.com.br PORTA: 80 e 443</pre>
+    </div>
+  </div>
+</div>
