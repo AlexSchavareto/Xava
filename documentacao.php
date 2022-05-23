e831e1410c7a946cf264644e6f916f67c7238d0e
Alex Schavareto <schavareto@yahoo.com.br>
2022-05-23 17:51:45 -0300

    documentação

--- documentacao.php
+++ documentacao.php
@@ -2,7 +2,9 @@
     require_once("assets/head.php");
     require_once("classes/classDocs.php");
 
-    $obj = new Documentos;
+    $obj = new DocumentosRest;
+    $obj = new DocumentosSoap;
+    $obj = new OutrosDocs;
     
 
 ?>
@@ -10,11 +12,6 @@
 
 <h1>Donwload Documentações</h1><br>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-
-<a href=<?php Documentos::smartlabeldoc_download(); ?>> Download </a><br>
-<a href=<?php Documentos::smartlabeldoc_visualizar(); ?> target="_blank"> Visualizar </a>
-
 </table>
 <hr>
 <h3>API's REST</h3> 
@@ -22,16 +19,15 @@
   <tbody>
     <tr>
     <th scope="col">SmartLabel</th>
-      <td><a href=<?php Documentos::smartlabeldoc_download(); ?>> Download </a><br></td>
-      <td><a href=<?php Documentos::smartlabeldoc_visualizar(); ?> target="_blank"> Visualizar </a></td>
+      <td><a href=<?php DocumentosRest::smartlabeldoc_download(); ?>> Download </a><br></td>
+      <td><a href=<?php DocumentosRest::smartlabeldoc_visualizar(); ?> target="_blank"> Visualizar </a></td>
     </tr>
     <tr>
     <th scope="col">Previsão de entrega</th>
-      <td><a href=<?php Documentos::apiprevisao_download(); ?>> Download </a><br></td>
-      <td><a href=<?php Documentos::apiprevisao_visualizar(); ?> target="_blank"> Visualizar </a></td>
+      <td><a href=<?php DocumentosRest::apiprevisao_download(); ?>> Download </a><br></td>
+      <td><a href=<?php DocumentosRest::apiprevisao_visualizar(); ?> target="_blank"> Visualizar </a></td>
     </tr>
   </tbody>
 </table>
-
 
 
