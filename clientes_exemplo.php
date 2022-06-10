873449015cc5eb2f3bbf54111adbc0590e55524f
Alex Schavareto <schavareto@yahoo.com.br>
2022-06-09 23:38:28 -0300

    Alteração das fontes

--- clientes_exemplo.php
+++ clientes_exemplo.php
@@ -21,20 +21,20 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingOne">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Notfis
+        <h4>Notfis</h4>
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-      <strong>NOTFIS 3.1</strong><br> - Cliente de Exemplo: <a href=<?php echo $notfisInt;?>target="_blank"> RIACHUELO</a><br>- Arquivo de Exemplo: <a href=> DOWNLOAD</a> <hr>
-      <strong>NOTFIS 5.0</strong> - <a href=<?php echo $notfis5;?>target="_blank"> MERCUR</a><br>
+      <strong><li>NOTFIS 3.1</li></strong> - Cliente de Exemplo: <a href=<?php echo $notfisInt;?>target="_blank"> RIACHUELO</a><br>- Arquivo de Exemplo: <a href=> DOWNLOAD</a> <hr>
+      <strong><li>NOTFIS 5.0</li></strong> - Cleinte de Exemplo: <a href=<?php echo $notfis5;?>target="_blank"> MERCUR</a><br>
     </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingTwo">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Ocorren
+        <h4>Ocorren</h4>
       </button>
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
@@ -47,7 +47,7 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingThree">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Intelipost
+        <h4>Intelipost</h4>
       </button>
     </h2>
     <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
@@ -64,7 +64,7 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingFour">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-        Declaração de Conteúdo
+        <h4>Declaração de Conteúdo</h4>
       </button>
     </h2>
     <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
